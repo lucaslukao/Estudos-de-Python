ae3a9eca0fe83a662d015f5a286482d37b072d2e
Lucas <lm195435@gmail.com>
2022-02-17 14:14:43 -0300

    Estudos-de-Python

---
+++ app_python/aula7_calculadora2.py
@@ -0,0 +1,18 @@
+class Calculadora:
+    def __init__(self):  # Desse jeito da para rodar sem o init
+        pass
+
+    def soma (self, valor_a, valor_b):
+        return valor_a + valor_b
+    def subtracao(self, valor_a, valor_b):
+        return valor_a - valor_b
+    def multiplicacao(self, valor_a, valor_b):
+        return valor_a * valor_b
+    def divisao(self, valor_a, valor_b):
+        return valor_a / valor_b
+
+calculadora = Calculadora()
+print(calculadora.soma(10, 2))
+print(calculadora.subtracao(5, 3))
+print(calculadora.divisao(100, 2))
+print(calculadora.multiplicacao(10, 5))
